a1d2d24a51688ed4305ac16b2327587dd2aee206
Zen <thedjaney@gmail.com>
2013-09-21 22:45:40 +0800

    increment master

--- app.js
+++ app.js
@@ -31,6 +31,9 @@ function ChatServer(io){
 			
 			socket.in(room).emit('initializeChatData',{users:users[room],history:history[room]});
 			socket.broadcast.to(room).emit('addChatUser',  {user:data,userId:socketId});
+			
+			socket.emit('chatMessage',{name:'System',message:'Welcome!',email:'chat@codertalks.com'});
+			
 		});
 		
 		socket.on('sendChat', function (data) {
